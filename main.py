86918be65da8d48d3f4f50957f16105ff7f09731
Abhishek UK <abhishekuk2255@gmail.com>
2024-04-18 20:48:52 +0530

    some basic optimizations

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ import easyocr
 import csv
 import uuid
 
+import time
 
 CUSTOM_MODEL_NAME = 'my_ssd_mobnet'
 PRETRAINED_MODEL_NAME = 'ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8'
@@ -49,7 +50,7 @@ detection_model = model_builder.build(model_config=configs['model'], is_training
 
 # Restore checkpoint
 ckpt = tf.compat.v2.train.Checkpoint(model=detection_model)
-ckpt.restore(os.path.join(paths['CHECKPOINT_PATH'], 'ckpt-10')).expect_partial()  # change checkpoint
+ckpt.restore(os.path.join(paths['CHECKPOINT_PATH'], 'ckpt-21')).expect_partial()  # change checkpoint
 
 @tf.function
 def detect_fn(image):
@@ -58,102 +59,82 @@ def detect_fn(image):
     detections = detection_model.postprocess(prediction_dict, shapes)
     return detections
 
-
 category_index = label_map_util.create_category_index_from_labelmap(files['LABELMAP'])
 
 
-# Detection function
-def detect_plate(img_path, min_score_thresh=.6):
-    IMAGE_PATH = img_path
-    img = cv2.imread(IMAGE_PATH)
-    image_np = np.array(img)
 
-    input_tensor = tf.convert_to_tensor(np.expand_dims(image_np, 0), dtype=tf.float32)
+# Plate Detection Function
+def detect_plate(img, detection_threshold=0.5, img_from_path=False):
+    if img_from_path: 
+        img_array = cv2.imread(img) # only for detect from direct img, if np array this dont need 
+    else:
+        img_array = img
+    
+    img_np = np.array(img_array)
+
+    input_tensor = tf.convert_to_tensor(np.expand_dims(img_np, 0), dtype=tf.float32)
     detections = detect_fn(input_tensor)
 
     num_detections = int(detections.pop('num_detections'))
     detections = {key: value[0, :num_detections].numpy() for key, value in detections.items()}
-    detections['num_detections'] = num_detections
-
-    # detection_classes should be ints.
     detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
 
-    label_id_offset = 1
-    image_np_with_detections = image_np.copy()
-
-    viz_utils.visualize_boxes_and_labels_on_image_array(
-        image_np_with_detections,
-        detections['detection_boxes'],
-        detections['detection_classes']+label_id_offset,
-        detections['detection_scores'],
-        category_index,
-        use_normalized_coordinates=True,
-        max_boxes_to_draw=1,
-        min_score_thresh=min_score_thresh, # min 0.8
-        agnostic_mode=False
-    )
-    return image_np_with_detections, detections
-
-
-img_with_plate, plate = detect_plate(os.path.join(paths['IMAGE_PATH'], 'test', 'Cars50.png'), 0.5)
-
-
-cv2.imwrite('sample1.png', img_with_plate)
-
-# import sys # to access the system
-# while True:
-#     cv2.imshow("Sheep", img_with_plate)
-#     cv2.waitKey(0)
-#     sys.exit() # to exit from all the processes
-
-
-
-def filter_text(region, ocr_result, region_threshold=0.6):
-    rectangle_size = region.shape[0]*region.shape[1]
-    plate = []
-    for result in ocr_result:
-        length = np.sum(np.subtract(result[0][1], result[0][0]))
-        height = np.sum(np.subtract(result[0][2], result[0][1]))
-        if length*height / rectangle_size > region_threshold:
-            plate.append(result[1])
-    return plate
-
-def ocr_it(image, detections, detection_threshold=0.7, region_threshold=0.6):
-    # Scores, boxes and classes above threhold
-    scores = list(filter(lambda x: x> detection_threshold, detections['detection_scores']))
+    # return img_np, detections
+    scores = list(filter(lambda x: x > detection_threshold, detections['detection_scores']))
+    # print(scores)
     boxes = detections['detection_boxes'][:len(scores)]
     classes = detections['detection_classes'][:len(scores)]
-    # Full image dimensions
-    width = image.shape[1]
-    height = image.shape[0]
-    # Apply ROI filtering and OCR
+
+    height = img_np.shape[0]
+    width = img_np.shape[1]
+    plates = []
+
     for idx, box in enumerate(boxes):
-        roi = box*[height, width, height, width]
-        region = image[int(roi[0]):int(roi[2]),int(roi[1]):int(roi[3])]
+        roi = box * [height, width, height, width]
+        region =  img_np[int(roi[0]) : int(roi[2]), int(roi[1]) : int(roi[3])]    
+        plates.append(region)
 
-        print(region.shape)
-        region = cv2.cvtColor(region, cv2.COLOR_BGR2GRAY) # may or may not need 
-        print(region.shape)
-
-        reader = easyocr.Reader(['en'])
-        ocr_result = reader.readtext(region)
-
-        text = filter_text(region, ocr_result, region_threshold)
-        return text, region
-
-
-text, region = ocr_it(img_with_plate, plate, detection_threshold=0.7, region_threshold=0.6)
-cv2.imwrite('sample5.png', region)
-
-print(f'text: {text}')
+    return plates
 
 
 
-def save_results(text, region, csv_filename, folder_path):
+# Function to Temp-Save data on csv file
+def save_results_as_csv(plate, text):
     img_name = '{}.jpg'.format(uuid.uuid1())
-    cv2.imwrite(os.path.join(folder_path, img_name), region)
-    with open(csv_filename, mode='a', newline='') as f:
+    cv2.imwrite(os.path.join('Detection_Images', img_name), plate)
+    with open('detection_results.csv', mode='a', newline='') as f:
         csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         csv_writer.writerow([img_name, text])
 
-save_results(text, region, 'detection_results.csv', 'Detection_Images')
+
+
+reader = easyocr.Reader(['en'], gpu=False)
+# Plate Recognition Function
+def rec_text(plates):
+    texts = []
+    for plate in plates:
+        ocr_result = reader.readtext(plate)
+        text = ''
+        for part in ocr_result:
+            text += part[1]     # 0.localization 1.text 2.accuracy
+        # print(text)
+        # save_results_as_csv(plate, text)
+        texts.append(text)
+    return texts
+
+
+
+
+
+
+
+
+print('started ')
+
+# img_with_plate, plate = detect_plate('Tensorflow/workspace/images/test/video2_610.jpg', 0.5)
+# text , region = ocr_it(img_with_plate, plate, detection_threshold=0.3, region_threshold=0.2)
+# print(text)
+# save_results(text, region, 'detection_results.csv', 'Detection_Images')
+
+
+print('done')
