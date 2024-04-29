f5c8a8d66ba14115d8e268723538f395124a4e1c
Abhishek UK <abhishekuk2255@gmail.com>
2024-04-29 07:04:47 +0530

    done with user search

--- app.py
+++ app.py
@@ -32,7 +32,8 @@ def admin_upload_vid():
     if request.method == 'POST':
         uploaded_file = request.files['file']
         if uploaded_file:
-            file_path = os.path.join('instance', 'uploads', uploaded_file.filename)
+            # file_path = os.path.join('instance', 'uploads', uploaded_file.filename)
+            file_path = os.path.join('static', 'uploads', uploaded_file.filename)
             uploaded_file.save(file_path)
             save_from_video(file_path)
     return render_template('admin-home.html', return_message='from_vid_upload')
@@ -42,6 +43,47 @@ def display_rec_db():
     mycursor.execute('select id, plate_number, loaction, date, time from recognized_vehicle')
     result = json.dumps(mycursor.fetchall())
     return render_template('admin-home.html', return_message='from_db_display', db_value=result)
+
+
+@app.route('/search/<number>')
+def search_plate(number):
+    mycursor.execute(f"select plate_number, loaction, hsrp, date, time, video_file_name, frame_time from recognized_vehicle where plate_number = '{number}';")
+    result = mycursor.fetchall()
+    
+    # mycursor.execute(f"SELECT black_listed FROM cars WHERE plate_number = '{number}'")
+    # black_listed = mycursor.fetchone()
+
+    # if black_listed or 5 == 10:
+    #     BL = 'Yes'
+    # else:
+    #     BL = 'No'
+        
+    if len(result) == 0:
+        return render_template('user-home.html',
+                                plate_not_found_section='''
+                                                    <div id="plate-not-found"> 
+                                                        <h1>The Plate is not Found</h1>
+                                                    </div>
+                                '''
+                )
+    
+    return render_template('user-home.html',
+                           plate_found_section=f'''
+                                            <div id="plate-found">
+                                                <div class="plate-details">
+                                                    <div class="grid-item" id="dis-plate-no">Plate Number: { result[0][0] }</div>
+                                                    <div class="grid-item" id="dis-location">Location: { result[0][1] }</div>
+                                                    <div class="grid-item" id="dis-hsrp">HSRP: { result[0][2] }</div>
+                                                    <div class="grid-item" id="dis-date">Date: { result[0][3] }</div>
+                                                    <div class="grid-item" id="dis-time">Time: { result[0][4] }</div>
+                                                    <div class="grid-item" id="dis-black-listed">Black listed: { 'No' }</div>
+                                                </div>
+                                            </div>
+                                                ''',
+                            display_variable='vid_db', 
+                            vid_file_name = result[0][5],
+                            start_time = result[0][6]
+            )
 
 
 
--- static/js/user-script.js
+++ static/js/user-script.js
@@ -3,10 +3,33 @@ const searchButton = document.getElementById("search-button");
 
 searchButton.addEventListener("click", function() {
   const searchTerm = searchInput.value;
-  // Add your search functionality here
-  alert("Searching for " + searchTerm);
+  Number_plate = searchTerm.replace(/\s/g, '')
+  window.location.href = '/search/' + Number_plate;
 });
 
 searchInput.addEventListener("input", function() {
   searchInput.value = searchInput.value.toUpperCase();
+});
+
+
+var video_div = document.getElementById('vid_db');
+    
+if(video_div) {
+    video_div.style.display = 'block';
+} else {
+    video_div.style.display = 'none';
+}
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  var video = document.getElementById("myVideo");
+  video.addEventListener("loadedmetadata", function() {
+    this.currentTime = parseInt(vid_start_time); // Start time in seconds (3 minutes 20 seconds)
+  });
+  video.addEventListener("timeupdate", function() {
+    if (this.currentTime >= parseInt(vid_start_time) + 15) { // End time in seconds (4 minutes 30 seconds)
+      this.pause();
+    }
+  });
 });
